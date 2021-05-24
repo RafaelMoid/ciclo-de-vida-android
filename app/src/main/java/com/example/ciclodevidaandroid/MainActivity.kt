018017f90d385c209f31cedc402b7b5dae9340e5
Rafael Varela <rafaelvrcr@gmail.com>
2021-05-24 09:30:58 -0300

    i've created some interest point on each part of my app lifecycle so this way i can notice when exactly they start working,that's fun, now i know more than yesterday :)

--- app/src/main/java/com/example/ciclodevidaandroid/MainActivity.kt
+++ app/src/main/java/com/example/ciclodevidaandroid/MainActivity.kt
@@ -2,11 +2,45 @@ package com.example.ciclodevidaandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
-
+    //onCreate, onStart, onPause são METODOS
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.w("Lifecycle", "entrei no onCreate() - Estou criando a tela")
     }
+
+    override fun onStart(){
+        super.onStart()
+        Log.w("Lifecycle", "entrei no onStart() - Deixei a tela visivel")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.w("Lifecycle", "entrei no onResume() - Agora você pode interagir com a tela")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.w("Lifecycle", "entrei no onPause() - A tela perdeu o foco, você não pode mais interagir")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.w("Lifecycle", "entrei no onStop() - A tela não está mais visivel mais ainda existe")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.w("Lifecycle", "entrei no onRestart() - A tela está retomando o foco")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w("Lifecycle", "entrei no onDestroy() - Ahh não, o usuario fechou o app")
+    }
+
+
 }
